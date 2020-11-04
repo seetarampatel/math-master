0dfcaf787ea3e76a29b3981fe7ccea84125cc56b
seetarampatel <200393396@student.georgianc.on.ca>
2020-11-03 23:26:50 -0500

    Firebase set up with sign-in authentication

---
+++ app/src/main/java/com/example/all_in_one_math/SignInActivity.kt
@@ -0,0 +1,52 @@
+package com.example.all_in_one_math
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+
+class SignInActivity : AppCompatActivity() {
+
+    private val RC_SIGN_IN = 6789;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+
+        // Authentication providers - Google and Email
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build())
+
+        // This will create and launch sign-in intent
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),
+            RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                // check for the successful sign-in of user
+                val user = FirebaseAuth.getInstance().currentUser
+                // send the user to Main activity
+                val intent = Intent(applicationContext, MainActivity::class.java)
+                startActivity(intent)
+            } else {
+                // If sign-in not happened successfully, then display the message to the user
+                Toast.makeText(this, "Invalid Login", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+}
