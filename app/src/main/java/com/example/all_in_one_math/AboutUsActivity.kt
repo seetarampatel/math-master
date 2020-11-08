3b3829878fc01c1a2e51fa4270188cb74b70a9a7
seetarampatel <200393396@student.georgianc.on.ca>
2020-11-08 09:26:04 -0500

    About Us Activity Implemented

---
+++ app/src/main/java/com/example/all_in_one_math/AboutUsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.all_in_one_math
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AboutUsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about_us)
+    }
+}
---
+++ app/src/main/java/com/example/all_in_one_math/FormulaActivity.kt
@@ -0,0 +1,11 @@
+package com.example.all_in_one_math
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FormulaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_formula)
+    }
+}
